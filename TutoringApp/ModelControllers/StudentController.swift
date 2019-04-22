761748d05fee93f66a9dea2996653070d125a4ca
Nathan Andrus <nathanpandrus@gmail.com>
2019-04-22 13:41:29 -0600

    Getting files in order and firebase

---
+++ TutoringApp/ModelControllers/StudentController.swift
@@ -0,0 +1,15 @@
+//
+//  StudentController.swift
+//  TutoringApp
+//
+//  Created by Nathan Andrus on 4/22/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class StudentController {
+    
+    
+}
---
+++ TutoringApp/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  TutoringApp
+//
+//  Created by Nathan Andrus on 4/22/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Student {
+    
+}
