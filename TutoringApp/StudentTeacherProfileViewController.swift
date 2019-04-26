90de0fe8493ae586369c027b4c202c4201b44466
Carson Buckley <carsonbuckley@GorillaBearz-Mac.local>
2019-04-26 13:42:09 -0600

    Bunch of outlets

---
+++ TutoringApp/StudentTeacherProfileViewController.swift
@@ -0,0 +1,48 @@
+//
+//  StudentTeacherProfileViewController.swift
+//  TutoringApp
+//
+//  Created by Carson Buckley on 4/26/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import UIKit
+
+class StudentTeacherProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var linkedInLabel: UILabel!
+    @IBOutlet weak var aboutMeTextView: UITextView!
+    @IBOutlet weak var qualificationsTextView: UITextView!
+    @IBOutlet weak var mondayTimeLabel: UILabel!
+    @IBOutlet weak var tuesdayTimeLabel: UILabel!
+    @IBOutlet weak var wednesdayTimeLabel: UILabel!
+    @IBOutlet weak var thursdayTimeLabel: UILabel!
+    @IBOutlet weak var fridayTimeLabel: UILabel!
+    @IBOutlet weak var saturdayTimeLabel: UILabel!
+    @IBOutlet weak var sundayTimeLabel: UILabel!
+    @IBOutlet weak var sendMessageButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func sendMessageButtonTapped(_ sender: Any) {
+    }
+    
+}
---
+++ TutoringApp/TeacherEditPreferencesViewController.swift
@@ -0,0 +1,133 @@
+//
+//  TeacherEditPreferencesViewController.swift
+//  TutoringApp
+//
+//  Created by Carson Buckley on 4/26/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import UIKit
+
+class TeacherEditPreferencesViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var settingsButton: UIBarButtonItem!
+    @IBOutlet weak var changePasswordButton: UIButton!
+    @IBOutlet weak var changeSubjectsButton: UIButton!
+    @IBOutlet weak var changePreferencesButton: UIButton!
+    @IBOutlet weak var changeBiographyButton: UIButton!
+    @IBOutlet weak var priceTextfield: UITextField!
+    @IBOutlet weak var mondayMorningButton: UIButton!
+    @IBOutlet weak var mondayAfternoonButton: UIButton!
+    @IBOutlet weak var mondayEveningButton: UIButton!
+    @IBOutlet weak var tuesdayMorningButton: UIButton!
+    @IBOutlet weak var tuesdayAfternoonButton: UIButton!
+    @IBOutlet weak var tuesdayEveningButton: UIButton!
+    @IBOutlet weak var wednesdayMorningButton: UIButton!
+    @IBOutlet weak var wednesdayAfternoonButton: UIButton!
+    @IBOutlet weak var wednesdayEveningButton: UIButton!
+    @IBOutlet weak var thursdayMorningButton: UIButton!
+    @IBOutlet weak var thursdayAfternoonButton: UIButton!
+    @IBOutlet weak var thursdayEveningButton: UIButton!
+    @IBOutlet weak var fridayMorningButton: UIButton!
+    @IBOutlet weak var fridayAfternoonButton: UIButton!
+    @IBOutlet weak var fridayEveningButton: UIButton!
+    @IBOutlet weak var saturdayMorningButton: UIButton!
+    @IBOutlet weak var saturdayAfternoonButton: UIButton!
+    @IBOutlet weak var saturdayEveningButton: UIButton!
+    @IBOutlet weak var sundayMorningButton: UIButton!
+    @IBOutlet weak var sundayAfternoonButton: UIButton!
+    @IBOutlet weak var sundayEveningButton: UIButton!
+    @IBOutlet weak var localButton: UIButton!
+    @IBOutlet weak var onlineButton: UIButton!
+    @IBOutlet weak var bothButton: UIButton!
+    @IBOutlet weak var saveChangesButton: UIButton!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func changePasswordButtonTapped(_ sender: Any) {
+    }
+    @IBAction func settingsButtonTapped(_ sender: Any) {
+    }
+    @IBAction func changeSubjectsButtonTapped(_ sender: Any) {
+    }
+    @IBAction func changePreferencesButtonTapped(_ sender: Any) {
+    }
+    @IBAction func mondayMorningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func mondayAfternoonButtonTapped(_ sender: Any) {
+    }
+    @IBAction func mondayEveningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func tuesdayMorningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func tuesdayAfternoonButtonTapped(_ sender: Any) {
+    }
+    @IBAction func tuesdayEveningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func wednesdayMorningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func wednesdayAfternoonButtonTapped(_ sender: Any) {
+    }
+    @IBAction func wednesdayEveningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func thursdayMorningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func thursdayAfternoonButtonTapped(_ sender: Any) {
+    }
+    @IBAction func thursdayEveningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func fridayMorningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func fridayAfternoonButtonTapped(_ sender: Any) {
+    }
+    @IBAction func fridayEveningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func saturdayMorningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func saturdayAfternoonButtonTapped(_ sender: Any) {
+    }
+    @IBAction func saturdayEveningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func sundayMorningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func sundayAfternoonButtonTapped(_ sender: Any) {
+    }
+    @IBAction func sundayEveningButtonTapped(_ sender: Any) {
+    }
+    @IBAction func localButtonTapped(_ sender: Any) {
+    }
+    @IBAction func onlineButtonTapped(_ sender: Any) {
+    }
+    @IBAction func bothButtonTapped(_ sender: Any) {
+    }
+    @IBAction func changeBiographyButtonTapped(_ sender: Any) {
+    }
+    @IBAction func saveChangesButtonTapped(_ sender: Any) {
+    }
+    
+   
+    
+}
