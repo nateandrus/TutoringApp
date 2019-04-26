b9dfa140ebdb20ccb9e6b1ef37459dfff9fc7a82
Nathan Andrus <nathanpandrus@gmail.com>
2019-04-26 16:29:59 -0600

    onboarding functionality

--- TutoringApp/Onboarding/SubjectsViewController.swift
+++ TutoringApp/Onboarding/SubjectsViewController.swift
@@ -28,8 +28,29 @@ class SubjectsViewController: UIViewController {
     
     var counting: Int = 0
     
+    var subjects: [String] = []
+    
+    //LandingPads
+    var nameLandingPad: String?
+    var email: String?
+    var profileImage: UIImage?
+    var location: String?
+    var dateOfBirth: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        businessManagementButton.backgroundColor = .lightGray
+        marketingButton.backgroundColor = .lightGray
+        productManagementButton.backgroundColor = .lightGray
+        entrepreneurshipButton.backgroundColor = .lightGray
+        economicsButton.backgroundColor = .lightGray
+        webdevelopmentButton.backgroundColor = .lightGray
+        uxdesignButton.backgroundColor = .lightGray
+        iosDevelopementButton.backgroundColor = .lightGray
+        javascriptButton.backgroundColor = .lightGray
+        pythonButton.backgroundColor = .lightGray
+        softwareEngineerButton.backgroundColor = .lightGray
+        accountingButton.backgroundColor = .lightGray
         numberSelectedLabel.text = "0 Selected"
         businessManagementButton.layer.cornerRadius = businessManagementButton.frame.height / 2
         accountingButton.layer.cornerRadius = accountingButton.frame.height / 2
@@ -46,53 +67,128 @@ class SubjectsViewController: UIViewController {
         nextStepButton.layer.cornerRadius = nextStepButton.frame.height / 2
     }
     
+    //Subject Booleans
+    var businessManagement: Bool = false
+    var accounting: Bool = false
+    var marketing: Bool = false
+    var productManagement: Bool = false
+    var entrepreneurship: Bool = false
+    var economics: Bool = false
+    var webDevelopement: Bool = false
+    var uxDesign: Bool = false
+    var iosDevelopement: Bool = false
+    var javascript: Bool = false
+    var python: Bool = false
+    var softwareDevelopement: Bool = false
+    
     //MARK: - Actions
     @IBAction func businessManagementButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: businessManagementButton)
+        if businessManagement == false {
+            turnButtonOn(button: businessManagementButton)
+            businessManagement = true
+        } else {
+            turnButtonOff(button: businessManagementButton)
+            businessManagement = false
+        }
     }
-    
     @IBAction func accountingButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: accountingButton)
+        if accounting == false {
+            turnButtonOn(button: accountingButton)
+            accounting = true
+        } else {
+            turnButtonOff(button: accountingButton)
+            accounting = false
+        }
     }
-    
     @IBAction func marketingButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: marketingButton)
+        if marketing == false {
+            turnButtonOn(button: marketingButton)
+            marketing = true
+        } else {
+            turnButtonOff(button: marketingButton)
+            marketing = false
+        }
     }
-    
     @IBAction func productManagementButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: productManagementButton)
+        if productManagement == false {
+            turnButtonOn(button: productManagementButton)
+            productManagement = true
+        } else {
+            turnButtonOff(button: productManagementButton)
+            productManagement = false
+        }
     }
-    
     @IBAction func entrprenuershipButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: entrepreneurshipButton)
+        if entrepreneurship == false {
+            turnButtonOn(button: entrepreneurshipButton)
+            entrepreneurship = true
+        } else {
+            turnButtonOff(button: entrepreneurshipButton)
+            entrepreneurship = false
+        }
     }
-    
     @IBAction func economicsButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: economicsButton)
+        if economics == false {
+            turnButtonOn(button: economicsButton)
+            economics = true
+        } else {
+            turnButtonOff(button: economicsButton)
+            economics = false
+        }
     }
-    
     @IBAction func webdevelopmentButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: webdevelopmentButton)
+        if webDevelopement == false {
+            turnButtonOn(button: webdevelopmentButton)
+            webDevelopement = true
+        } else {
+            turnButtonOff(button: webdevelopmentButton)
+            webDevelopement = false
+        }
     }
-    
     @IBAction func uxDesignButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: uxdesignButton)
+        if uxDesign == false {
+            turnButtonOn(button: uxdesignButton)
+            uxDesign = true
+        } else {
+            turnButtonOff(button: uxdesignButton)
+            uxDesign = false
+        }
     }
-    
     @IBAction func iosDevelopmentButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: iosDevelopementButton)
+        if iosDevelopement == false {
+            turnButtonOn(button: iosDevelopementButton)
+            iosDevelopement = true
+        } else {
+            turnButtonOff(button: iosDevelopementButton)
+            iosDevelopement = false
+        }
     }
-    
     @IBAction func javascriptButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: javascriptButton)
+        if javascript == false {
+            turnButtonOn(button: javascriptButton)
+            javascript = true
+        } else {
+            turnButtonOff(button: javascriptButton)
+            javascript = false
+        }
     }
-    
     @IBAction func pythonButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: pythonButton)
+        if python == false {
+            turnButtonOn(button: pythonButton)
+            python = true
+        } else {
+            turnButtonOff(button: pythonButton)
+            python = false
+        }
     }
-    
     @IBAction func softwareEngineerButtonTapped(_ sender: UIButton) {
-        turnButtonOn(button: softwareEngineerButton)
+        if softwareDevelopement == false {
+            turnButtonOn(button: softwareEngineerButton)
+            softwareDevelopement = true
+        } else {
+            turnButtonOff(button: softwareEngineerButton)
+            softwareDevelopement = false
+        }
     }
     
     func turnButtonOn(button: UIButton) {
@@ -103,7 +199,7 @@ class SubjectsViewController: UIViewController {
     }
     
     func turnButtonOff(button: UIButton) {
-        button.backgroundColor = .white
+        button.backgroundColor = .lightGray
         button.setTitleColor(.black, for: .normal)
         self.counting = counting - 1
         self.numberSelectedLabel.text = "\(counting) Selected"
