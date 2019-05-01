970a16fa368ae2da89d99bb65fe4ce673fbefc73
Carson Buckley <carsonbuckley@GorillaBearz-Mac.local>
2019-05-01 16:40:29 -0600

    bunch of changes and shit

--- TutoringApp/StudentSearchTableViewController.swift
+++ TutoringApp/StudentSearchTableViewController.swift
@@ -9,97 +9,94 @@
 import UIKit
 
 class StudentSearchTableViewController: UITableViewController {
-
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    
-    
+    var teachers: [Teacher] = []
+    var subject: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchBar.delegate = self
         segmentedControl.layer.cornerRadius = 6
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        guard let subject = subject else { return }
+        StudentController.shared.searchBySubject(subject: subject) { (success) in
+            if success {
+                self.tableView.reloadData()
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         segmentedControl.index(ofAccessibilityElement: 0)
         segmentedControl.index(ofAccessibilityElement: 1)
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    @IBAction func filterButtonTapped(_ sender: Any) {
+        return StudentController.shared.searchResults.count
     }
     
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "teacherCell", for: indexPath) as! StudentSearchTableViewCell
+        
+        let teacher = StudentController.shared.searchResults[indexPath.row]
+        
+        cell.teacher = teacher
+        
+        return cell
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBAction func filterButtonTapped(_ sender: Any) {
+        
+    }
 }
+
+extension StudentSearchTableViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let search = searchBar.text else { return }
+        StudentController.shared.searchTeacherWithID(searchTerm: search) { (teacher) in
+           
+        }
+    }
+}
+
+//po StudentController.shared.searchResults
+//▿ 1 element
+//    ▿ 0 : Teacher
+//- name : "johnny"
+//- email : "johnnyyy@gmail.com"
+//▿ messages : Optional<Array<FIRDocumentReference>>
+//- some : 0 elements
+//- firebaseUID : "gK8ihhAttlPOYWaOMtTed0DoZfu1"
+//▿ linkedINLink : Optional<String>
+//- some : "www.linkedin.com/johnnyyyyy"
+//- costForTime : "33"
+//- qualifications : "How goes it qualificationssssssssss"
+//- location : "highland, Utah "
+//- dateOfBirth : "may "
+//▿ subjects : 2 elements
+//- 0 : "Accounting"
+//- 1 : "Entrepreneurship"
+//▿ schedulePref : 2 elements
+//- 0 : "FridayAfternoon"
+//- 1 : "MondayEvening"
+//- meetingPref : "Both"
+//- aboutMe : "This is an about you johnnnnyyyyyy boy \t"
+//- profileImage : nil
+//▿ profileImageURL : Optional<String>
+//- some : "gK8ihhAttlPOYWaOMtTed0DoZfu1"
+//- selfDocRef : <FIRDocumentReference: 0x6000032e3ee0>
